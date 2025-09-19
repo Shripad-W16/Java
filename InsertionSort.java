4a61b9572abd98c424f9b09fd480b1743a6992c0
Shripad-W16 <shripadingle11@gmail.com>
2025-09-19 19:46:16 +0530

    Create InsertionSort.java

---
+++ InsertionSort.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class InsertionSort {
+public static void main(String[] args) {
+Scanner sc = new Scanner(System.in);
+
+// Input size of array
+System.out.print("Enter number of elements: ");
+int n = sc.nextInt();
+
+int[] arr = new int[n];
+
+// Input array elements
+System.out.println("Enter " + n + " numbers:");
+for (int i = 0; i < n; i++) {
+arr[i] = sc.nextInt();
+}
+
+// Insertion sort
+for (int i = 1; i < arr.length; i++) {
+int key = arr[i];
+int j = i - 1;
+
+while (j >= 0 && arr[j] > key) {
+arr[j + 1] = arr[j];
+j--;
+}
+arr[j + 1] = key;
+}
+
+// Print sorted array
+System.out.println("Sorted array:");
+for (int num : arr) {
+System.out.print(num + " ");
+}
+
+sc.close();
+}
+}
